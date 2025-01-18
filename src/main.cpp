87036daf443eb4ec19b94760cd722338bfccb621
kimel1992 <kimelgever@gmail.com>
2025-01-18 19:21:24 +0200

    Finished with cmake infra, frames handling still not working

--- src/main.cpp
+++ src/main.cpp
@@ -1,53 +1,15 @@
 #include <hailo/hailort.hpp>
-#include <opencv2/opencv.hpp>
+
 #include <iostream>
+
+#include "frames/get_frames.hpp"
 
 int main()
 {
     auto dev = hailort::Device::scan().expect("Failed getting a device");
     std::cout << dev.size() << std::endl;
 
-    cv::VideoCapture cap(0); // 0 is the default device (Raspberry Pi camera)
-
-    // Check if the camera opened successfully
-    if (!cap.isOpened()) {
-        std::cerr << "Error: Could not open the camera." << std::endl;
-        return -1;
-    }
-
-    // Get the frames per second (fps) of the camera
-    double fps = cap.get(cv::CAP_PROP_FPS);
-    if (fps == 0) {
-        std::cout << "Warning: Could not retrieve FPS, using default (30)." << std::endl;
-        fps = 30.0;
-    }
-
-    // Frame container
-    cv::Mat frame;
-
-    // Capture and display video frames
-    while (true) {
-        // Capture a frame from the camera
-        cap >> frame;
-
-        // Check if the frame is empty (end of video)
-        if (frame.empty()) {
-            std::cerr << "Error: Could not grab a frame." << std::endl;
-            break;
-        }
-
-        // Display the frame
-        cv::imshow("Video Stream", frame);
-
-        // Wait for 'q' key to exit
-        if (cv::waitKey(1000 / fps) == 'q') {
-            break;
-        }
-    }
-
-    // Release the camera and close the display window
-    cap.release();
-    cv::destroyAllWindows();
+    auto cam = CameraCapture();
 
     return 0;
 }
